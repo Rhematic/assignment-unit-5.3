e8135385f4f6e7088615e4607e27abf754c8fbff
Adam Myhand <rhematicmovement@gmail.com>
2023-07-05 14:24:48 -0500

    Finished

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -21,6 +21,11 @@ function addToCollection(title, artist, yearPublished) {
   return album;
 }
 
+// - Test the `addToCollection` function:
+//   - Add 6 albums to your collection. Aim to have a mix of both same and different artists and published years. (Feel free to share your musical interests, or make stuff up. Totally fine either way.)
+//   - Console.log each album as added using the returned value.
+//   - After all are added, console.log the `collection` array.
+
 addToCollection("U Remind Me", "Usher", "2001");
 addToCollection("U Got It Bad", "Usher", "2001");
 addToCollection("U Don't Have to Call", "Usher", "2001");
@@ -30,24 +35,46 @@ addToCollection("Green Onions", "Booker T. & The M.G.'s", "1962");
 
 console.log(collection);
 
-// - Test the `addToCollection` function:
-//   - Add 6 albums to your collection. Aim to have a mix of both same and different artists and published years. (Feel free to share your musical interests, or make stuff up. Totally fine either way.)
-//   - Console.log each album as added using the returned value.
-//   - After all are added, console.log the `collection` array.
-
 // - Add a function named `showCollection`. This function should:
-//   - Take in an array parameter. (This allows it to be reused to show any collection, like the results from the find or search.)
+//   - Take in an array parameter. (This allows it to be reused to show any collection,
+// like the results from the find or search.)
 //   - Console.log the number of items in the array.
-//   - Loop over the array and console.log each album's information formatted like: `TITLE by ARTIST, published in YEAR`.
+//   - Loop over the array and console.log each album's information formatted like:
+// `TITLE by ARTIST, published in YEAR`.
+
+function showCollection(array) {
+  console.log(array.length);
+  for (const property of array) {
+    console.log(`${property.title} by ${property.artist}, 
+    published in ${property.yearPublished}`);
+  }
+}
 
 // - Test the `showCollection` function.
+
+showCollection(collection);
 
 // - Add a function named `findByArtist`. This function should:
 //   - Take in `artist` (a string) parameter
 //   - Create an array to hold any results, empty to start
-//   - Loop through the `collection` and add any objects with a matching artist to the array.
-//   - Return the array with the matching results. If no results are found, return an empty array.
+//   - Loop through the `collection` and add any objects with a matching
+// artist to the array.
+//   - Return the array with the matching results. If no results are found,
+// return an empty array.
+
+function findByArtist(artist) {
+  let artistArray = [];
+  for (const song of collection) {
+    if (song.artist === artist) {
+      artistArray.push(song);
+    }
+  }
+  return artistArray;
+}
 
 // - Test the `findByArtist` function. Make sure to test with an artist you know is in the collection, as well as an artist you know is not in your collection. Check that for artists with multiple matches, all are found.
+
+console.log(findByArtist("Dolly Parton"));
+console.log(findByArtist("Usher"));
 
 // > When testing your functions, write all tests in the JavaScript file!
